7e094d9f7be97702557508210ad6203b18be7552
Wes Hardee <weshardee@gmail.com>
2016-02-17 22:52:41 -0600

    select breeding pairs

--- app/population.js
+++ app/population.js
@@ -5,6 +5,8 @@ import {
 
 const POP_SIZE = 50;
 const debug = require('debug')('app:population');
+const NUM_ELITE_CLONES = 2;
+
 
 export function createPopulation() {
     let population = [];
@@ -22,6 +24,32 @@ export function sortPopulation(pop) {
 }
 
 export function breed(pop) {
+    // grab some elites to clone into the next population
+    const newPop = pop.slice(0, NUM_ELITE_CLONES);
+
+    // select mating pairs
+    const unpicked = pop.slice();
+    while (unpicked.length > 0) {
+        const select = (percent) => pop.find()
+        const critterA = removeByRoulette(unpicked);
+        const critterB = removeByRoulette(unpicked);
+        const pair = [ critterA, critterB ];
+        debug('breeding pair selected', pair);
+    }
+
+    return newPop;
+}
+
+function removeByRoulette(pop) {
+    // get the total score for roulette wheel mating
     const totalScore = pop.reduce((total, critter) => total + critter.score, 0);
-    debug('population total', totalScore);
+    const spin = Math.random() * totalScore;
+
+    let runningScore = 0;
+    const i = pop.findIndex(critter => {
+        runningScore += critter.score;
+        return runningScore > spin;
+    });
+
+    return pop.splice(i, 1);
 }
